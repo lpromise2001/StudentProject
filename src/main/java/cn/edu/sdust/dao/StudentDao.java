15876491dde528423664433d244adc073ecb49bb
lpromise2001 <lpromise2001@163.com>
2021-11-21 20:45:46 +0800

    update

---
+++ src/main/java/cn/edu/sdust/dao/StudentDao.java
@@ -0,0 +1,24 @@
+package cn.edu.sdust.dao;
+
+import cn.edu.sdust.pojo.Student;
+
+import java.util.List;
+
+public interface StudentDao {
+
+    //增加一个学生
+    int addStudent(Student student);
+
+    //根据学号删除一个学生
+    int deleteStudentById(int id);
+
+    //通过学号查询学生
+    Student queryStudentById(int id);
+
+    //查询所有学生
+    List<Student> queryAllStudent();
+
+    //更改学生信息
+    int updateStudent(Student student, int id);
+
+}
---
+++ web/WEB-INF/jsp/home_page.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: LIULINHAO
+  Date: 2021/11/14
+  Time: 20:11
+  To change this template use File | Settings | File Templates.
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Home</title>
+    H2
+</head>
+
+
+<body>
+
+</body>
+</html>
